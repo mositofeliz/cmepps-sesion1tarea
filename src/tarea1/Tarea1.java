a43978b81cdc9c3b8eb5c2e6dfc8e96d34538792
mositofeliz <adrianalvarez_1997@hotmail.com>
2022-10-10 21:29:15 +0200

    Primer commit del proyecto

---
+++ src/tarea1/Tarea1.java
@@ -0,0 +1,17 @@
+package tarea1;
+
+/**
+ *
+ * @author adri
+ */
+public class Tarea1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Empezamos!");
+    }
+    
+}
